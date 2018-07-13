b5822601a699edc7b91c40381655b5fe4584090d
Gavin Price <gaprice@lbl.gov>
2018-07-12 22:00:42 -0700

    Add utils class & tests
    Can test by just running `pytest` in src

---
+++ src/jgikbase/idmapping/util/Util.py
@@ -0,0 +1,24 @@
+'''
+
+Utility functions
+
+@author: gaprice@lbl.gov
+'''
+from typing import cast
+
+
+def not_none(obj: object, name: str):
+    '''
+    Check if an object is not None. In the case of a string, the string must also contain
+    non-whitespace characters.
+
+    If the object is None or a string is whitespace-only, a ValueError will be thrown.
+
+    :param obj: the object to check
+    :param name: the name of the object to use in error messages.
+    '''
+    if type(obj) == str:
+        if not cast(str, obj).strip():
+            raise ValueError(name + ' cannot be None or whitespace only')
+    elif not obj:
+        raise ValueError(name + ' cannot be None')
---
+++ src/jgikbase/test/idmapping/util/util_test.py
@@ -0,0 +1,33 @@
+'''
+
+@author: gaprice@lbl.gov
+'''
+
+from jgikbase.idmapping.util.Util import not_none
+from jgikbase.test.idmapping.test_utils import assert_exception_correct
+import pytest
+
+
+def test_no_none_obj_pass():
+    not_none(4, 'integer')
+
+
+def test_no_none_string_pass():
+    not_none('four', 'text')
+
+
+def test_no_none_obj_fail():
+    fail_no_none(None, 'my name', ValueError('my name cannot be None'))
+
+
+def test_no_none_str_fail():
+    fail_no_none('   \t   \n   ', 'my name',
+                 ValueError('my name cannot be None or whitespace only'))
+
+
+def fail_no_none(obj: object, name: str, expected: Exception):
+    try:
+        not_none(obj, name)
+        pytest.fail('expected exception')
+    except Exception as got:
+        assert_exception_correct(got, expected)
