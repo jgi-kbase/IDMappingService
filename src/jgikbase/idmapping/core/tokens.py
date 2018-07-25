9573dc013b5eaaf520e7d086925e51b324fae1bd
Gavin Price <gaprice@lbl.gov>
2018-07-25 15:39:21 -0700

    Add Token class & hashing and generation code.

--- src/jgikbase/idmapping/core/tokens.py
+++ src/jgikbase/idmapping/core/tokens.py
@@ -3,6 +3,9 @@ Classes representing authentication tokens.
 """
 
 from jgikbase.idmapping.core.util import check_string
+import secrets
+import hashlib
+import base64
 
 
 class HashedToken:
@@ -21,3 +24,33 @@ class HashedToken:
         """
         check_string(token_hash, 'token_hash')
         self.token_hash = token_hash
+
+
+class Token:
+    """
+    A token.
+
+    :ivar token: the token.
+    """
+
+    def __init__(self, token) -> None:
+        '''
+        Create a token.
+        :param token: the token string.
+        :raises MissingParameterError: if the token is None or whitespace only.
+        '''
+        check_string(token, "token")
+        self.token = token
+
+    def get_hashed_token(self) -> HashedToken:
+        '''
+        Returns a sha256 hash of this token represented as a hex string.
+        '''
+        return HashedToken(hashlib.sha256(self.token.encode()).hexdigest())
+
+
+def generate_token() -> Token:
+    '''
+    Generates a 160 bit token in base64 format.
+    '''
+    return Token(base64.b64encode(secrets.token_bytes(20)))
--- src/jgikbase/test/idmapping/core/tokens_test.py
+++ src/jgikbase/test/idmapping/core/tokens_test.py
@@ -1,7 +1,9 @@
-from jgikbase.idmapping.core.tokens import HashedToken
+from jgikbase.idmapping.core.tokens import HashedToken, Token
 from pytest import fail
 from jgikbase.test.idmapping.test_utils import assert_exception_correct
 from jgikbase.idmapping.core.errors import MissingParameterError
+from jgikbase.idmapping.core import tokens
+import base64
 
 
 def test_hashed_token_init_pass():
@@ -20,3 +22,42 @@ def fail_hashed_token_init(htoken: str, expected: Exception):
         fail('expected exception')
     except Exception as got:
         assert_exception_correct(got, expected)
+
+
+def test_token_init_pass():
+    t = Token('foo')
+    assert t.token == 'foo'
+
+
+def test_token_init_fail():
+    fail_token_init(None, MissingParameterError('token'))
+    fail_token_init('   \t    \n   ', MissingParameterError('token'))
+
+
+def fail_token_init(token: str, expected: Exception):
+    try:
+        Token(token)
+        fail('expected exception')
+    except Exception as got:
+        assert_exception_correct(got, expected)
+
+
+def test_hash_token():
+    t = Token('foo')
+    ht = t.get_hashed_token()
+    assert ht.token_hash == '2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae'
+
+
+def test_generate_token():
+    t = tokens.generate_token()
+    assert isBase64(t.token) is True
+    assert len(t.token) is 28
+
+
+def isBase64(s):
+    try:
+        if base64.b64encode(base64.b64decode(s)) == s:
+            return True
+    except Exception as e:
+        print(e)
+    return False
