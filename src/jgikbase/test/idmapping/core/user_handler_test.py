e460fe83592981f668e27003336ad5e69deeb6d6
Gavin Price <gaprice@lbl.gov>
2018-07-31 18:51:41 -0700

    Add a user handler set with caching
    TODOs mark stuff that still needs tests. Have the basic caching tests
    working.

--- src/jgikbase/test/idmapping/core/user_handler_test.py
+++ src/jgikbase/test/idmapping/core/user_handler_test.py
@@ -1,30 +1,139 @@
 from unittest.mock import create_autospec
 from jgikbase.idmapping.storage.id_mapping_storage import IDMappingStorage
-from jgikbase.idmapping.core.user_handler import LocalUserHandler
+from jgikbase.idmapping.core.user_handler import LocalUserHandler, UserHandlerSet, UserHandler
 from jgikbase.idmapping.core.user import AuthsourceID, User, Username
 from jgikbase.idmapping.core.tokens import Token, HashedToken
 from jgikbase.test.idmapping.test_utils import assert_exception_correct
 from pytest import raises
 from jgikbase.test.idmapping.core.tokens_test import is_base64
+import time
 
 
-def test_init_fail():
+def test_set_init_fail():
+    handler = create_autospec(UserHandler, spec_set=True, instance=True)
+
+    fail_set_init(None, TypeError('user_handlers cannot be None'))
+    fail_set_init(set([handler, None]), TypeError('None item in user_handlers'))
+
+
+def fail_set_init(handlers, expected):
+    with raises(Exception) as got:
+        UserHandlerSet(handlers)
+    assert_exception_correct(got.value, expected)
+
+
+def test_set_get_user_default_cache_ttl():
+    handler = create_autospec(UserHandler, spec_set=True, instance=True)
+    timer = create_autospec(time.time, spec_set=True)
+    handler.get_authsource_id.return_value = AuthsourceID('as')
+
+    hset = UserHandlerSet(set([handler]), timer)
+
+    handler.get_user.return_value = (User(AuthsourceID('as'), Username('u')), False, None, None)
+    timer.return_value = 0
+
+    # user will not be in cache
+    assert hset.get_user(AuthsourceID('as'), Token('t')) == \
+        (User(AuthsourceID('as'), Username('u')), False)
+
+    # user is now cached
+    handler.get_user.return_value = None  # should cause error if called from now on
+    timer.return_value = 299  # just below default cache time of 300
+
+    assert hset.get_user(AuthsourceID('as'), Token('t')) == \
+        (User(AuthsourceID('as'), Username('u')), False)
+
+    # now expire the user
+    handler.get_user.return_value = (User(AuthsourceID('as'), Username('u')), True, None, None)
+    timer.return_value = 300
+
+    assert hset.get_user(AuthsourceID('as'), Token('t')) == \
+        (User(AuthsourceID('as'), Username('u')), True)
+
+    # get the user again, should be cached.
+    handler.get_user.return_value = None  # should cause error if called from now on
+    timer.return_value = 301
+
+    assert hset.get_user(AuthsourceID('as'), Token('t')) == \
+        (User(AuthsourceID('as'), Username('u')), True)
+
+    assert handler.get_user.call_args_list == [((Token('t'),), {}), ((Token('t'),), {})]
+
+
+def test_set_is_valid_user_default_cache_ttl():
+    handler = create_autospec(UserHandler, spec_set=True, instance=True)
+    timer = create_autospec(time.time, spec_set=True)
+    handler.get_authsource_id.return_value = AuthsourceID('as')
+
+    hset = UserHandlerSet(set([handler]), timer)
+
+    handler.is_valid_user.return_value = (True, None, None)
+    timer.return_value = 0
+
+    # user will not be in cache
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is True
+
+    # user is now cached
+    handler.is_valid_user.return_value = None  # should cause error if called from now on
+    timer.return_value = 3599  # just below default cache time of 3600
+
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is True
+
+    # now expire the user
+    handler.is_valid_user.return_value = (True, None, None)
+    timer.return_value = 3600
+
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is True
+
+    # get the user again, should be cached
+    handler.is_valid_user.return_value = None  # should cause error if called from now on
+    timer.return_value = 3601
+
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is True
+
+    assert handler.is_valid_user.call_args_list == [((Username('u'),), {}), ((Username('u'),), {})]
+
+
+def test_set_is_valid_user_invalid_user():
+    # invalid users shouldn't get cached.
+    handler = create_autospec(UserHandler, spec_set=True, instance=True)
+    timer = create_autospec(time.time, spec_set=True)
+    handler.get_authsource_id.return_value = AuthsourceID('as')
+
+    hset = UserHandlerSet(set([handler]), timer)
+
+    handler.is_valid_user.return_value = (False, None, None)
+    timer.return_value = 0
+
+    # user will not be in cache
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is False
+
+    # would normally expect a cache time of 3600s, but should not be cached here.
+
+    timer.return_value = 10
+
+    assert hset.is_valid_user(User(AuthsourceID('as'), Username('u'))) is False
+
+    assert handler.is_valid_user.call_args_list == [((Username('u'),), {}), ((Username('u'),), {})]
+
+
+def test_local_init_fail():
     with raises(Exception) as got:
         LocalUserHandler(None)
     assert_exception_correct(got.value, TypeError('storage cannot be None'))
 
 
-def test_get_authsource():
+def test_local_get_authsource():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     assert LocalUserHandler(storage).get_authsource_id() == AuthsourceID('local')
 
 
-def test_get_user_admin():
-    check_get_user_admin(True)
-    check_get_user_admin(False)
+def test_local_get_user_admin():
+    check_local_get_user_admin(True)
+    check_local_get_user_admin(False)
 
 
-def check_get_user_admin(isadmin):
+def check_local_get_user_admin(isadmin):
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     storage.get_user.return_value = (Username('bar'), isadmin)
 
@@ -35,14 +144,14 @@ def check_get_user_admin(isadmin):
     assert storage.get_user.call_args_list == [((HashedToken(thash),), {})]
 
 
-def test_get_user_fail():
+def test_local_get_user_fail():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     with raises(Exception) as got:
         LocalUserHandler(storage).get_user(None)
     assert_exception_correct(got.value, TypeError('token cannot be None'))
 
 
-def test_is_valid_user():
+def test_local_is_valid_user():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     storage.user_exists.return_value = True
 
@@ -59,14 +168,14 @@ def test_is_valid_user():
         ((Username('bar'),), {})]
 
 
-def test_is_valid_user_fail():
+def test_local_is_valid_user_fail():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     with raises(Exception) as got:
         LocalUserHandler(storage).is_valid_user(None)
     assert_exception_correct(got.value, TypeError('username cannot be None'))
 
 
-def test_create_user():
+def test_local_create_user():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
 
     t = LocalUserHandler(storage).create_user(Username('foo'))
@@ -78,14 +187,14 @@ def test_create_user():
         [((Username('foo'), t.get_hashed_token()), {})]
 
 
-def test_create_user_fail():
+def test_local_create_user_fail():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     with raises(Exception) as got:
         LocalUserHandler(storage).create_user(None)
     assert_exception_correct(got.value, TypeError('username cannot be None'))
 
 
-def test_new_token():
+def test_local_new_token():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
 
     t = LocalUserHandler(storage).new_token(Username('bar'))
@@ -97,14 +206,14 @@ def test_new_token():
         [((Username('bar'), t.get_hashed_token()), {})]
 
 
-def test_new_token_fail():
+def test_local_new_token_fail():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     with raises(Exception) as got:
         LocalUserHandler(storage).new_token(None)
     assert_exception_correct(got.value, TypeError('username cannot be None'))
 
 
-def test_get_users():
+def test_local_get_users():
     storage = create_autospec(IDMappingStorage, spec_set=True, instance=True)
     storage.get_users.return_value = {Username('foo'): False, Username('bar'): True}
 
