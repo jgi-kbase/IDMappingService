991787a437f9db866bd4236fe9733039183a5276
Sijie <sijiex@lbl.gov>
2024-06-18 10:35:16 -0700

    add debug message

--- src/jgikbase/test/idmapping/integration/service_test.py
+++ src/jgikbase/test/idmapping/integration/service_test.py
@@ -123,6 +123,10 @@ def test_root(service_port):
     r = requests.get('http://localhost:' + service_port)
     j = r.json()
 
+    print("-----------")
+    print(j)
+    print("-----------")
+
     time_ = j['servertime']
     commit = j['gitcommithash']
     del j['servertime']
